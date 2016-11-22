6a25793add8c3503446631bb612d6d80f77b3908
Daniel Koch <xuedi@beijingcode.org>
2016-11-22 19:12:08 +0100

    dedicated space for globals like debug and php settings

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
 
-// some basic php settings & error/dbg functions
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
-ini_set("html_errors", 1); 
-ini_set("error_prepend_string", "<pre style='color: #333; font-face:monospace; font-size:12pt;'>"); 
-ini_set("error_append_string ", "</pre>"); 
-function dbg($data, $die=false) {
-	$backtrace = debug_backtrace();
-	echo "<pre style='border: 2px solid black; margin: 10px; padding: 7px; '>";
-	echo "<b>dbg</b> called from '".$backtrace[0]['file']."' line: ".$backtrace[0]['line']."<br>";
-	echo "<pre style='background-color: #eee; padding: 7px; '>".print_r($data,true)."</pre>";
-	echo "</pre>";
-	if($die) die();
-}
 
 // get the locations right
 $lib = realpath(dirname(__FILE__));
@@ -21,6 +8,7 @@ $app = realpath(dirname($_SERVER["SCRIPT_FILENAME"]).'/..');
 
 // autoload
 define('startTime', microtime(true));
+require $lib.'/app/globals.php';
 require $lib.'/vendor/autoload.php';
 $pebug = \Pedetes\pebug::Instance();
 $pebug->init(startTime);
