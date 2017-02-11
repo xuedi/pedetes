d3a2011d7a84fca56aaf62cfe88d01fe8f4f69f6
Daniel Koch <daniel.koch@vergleich.org>
2017-02-11 14:11:21 +0100

    reworked the request model back to simplicity

--- app/request.php
+++ app/request.php
@@ -3,168 +3,149 @@ namespace Pedetes;
 // http://php.net/manual/en/filter.filters.sanitize.php
 // http://php.net/manual/en/filter.filters.validate.php
 
-//Todo: complete cleanup
+use HTMLPurifier;
+
 class request {
 
     private $ctn;
     /** @var  pebug $pebug */
     private $pebug;
 
-    private $returnValue;
-    private $strict;
-    private $type;
-    private $default;
-    private $isValidated;
-    private $hadValidation;
-    private $array;
+    private $strict = null;
 
     function __construct($ctn) {
         $this->pebug = $ctn['pebug'];
         $this->pebug->log("request::__construct()");
 
         $this->ctn = $ctn;
-        $this->type = null;
-        $this->array = array();
-        $this->default = null;
         $this->strict = false;
-        $this->hadValidation = false;
-        $this->isValidated = false;
     }
 
-    public function setMock($parameter, $value=null) {
-        $_REQUEST[$parameter] = $value;
+    /**
+     * Inject varaibles for unittesting and so on
+     * @param $name string Name of the parameter
+     * @param $value null The value to be set as a parameter
+     */
+    public function setMock($name, $value=null) {
+        $_REQUEST[$name] = $value;
     }
 
-    public function name($name) {
-        $this->returnValue = $_REQUEST[$name] ?? null;
-        return $this;
-    }
-
-    public function strict() {
+    /**
+     * Set mode to strict, if error occours, system stops with and exception
+     * If not in strict mode system will give back the default value
+     */
+    public function setStrict() {
         $this->strict = true;
-        return $this;
     }
 
-    public function default($name='') {
-        $this->default = $name;
-        return $this;
-    }
-
-    public function array($array) {
-        $this->array = $array;
-        return $this;
-    }
-
-    public function validatePlaintext() {
-        $this->type = 'plaintext';
-        $this->hadValidation = true;
-        $this->isValidated = true; //Todo: just allow [a-z], [A-Z], [0-9], and so on
-        return $this;
-    }
-
-    public function validateFree() { // for passwords, should only be compared by PHP. never by statements, should trigger warning on use
-        $this->type = 'free';
-        $this->hadValidation = true;
-        $this->isValidated = true; //Todo: check of suspicious stuff
-        return $this;
-    }
-
-    public function validateEmail() {
-        $this->type = 'email';
-        $this->hadValidation = true;
-        $this->isValidated = filter_var($this->returnValue, FILTER_SANITIZE_EMAIL);
-        return $this;
-    }
-
-    public function validateArray() {
-        $this->type = 'array';
-        $this->hadValidation = true;
-        if(empty($this->array)) {
-            $this->isValidated = false;
-            return $this;
-        }
-        if(in_array($this->returnValue,$this->array)) {
-            $this->isValidated = true;
-        } else {
-            $this->isValidated = false;
-        }
-        return $this;
-    }
-
-    public function validateNumber() {
-        $this->type = 'number';
-        $this->hadValidation = true;
-        $this->isValidated = is_numeric($this->returnValue);
-        return $this;
-    }
-
-    public function value() {
-        if(!isset($this->returnValue)) {
-            $this->pebug->exception("request::_get->NoNameWasGiven");
-        }
-        if(!$this->hadValidation) {
-            $this->pebug->exception("request::_get->ValidationIsMissing");
-        }
-        if($this->strict) { // strict, just trough exception
-            if (!$this->isValidated) {
-                $this->pebug->exception("request::_get->ValidationFailed");
-            }
-        } else { // not strict, try default
-            if($this->default===null) {
-                $this->pebug->exception("request::_get->NoDefaultWasGiven");
+    /**
+     * @param $name string The name of the requested paraameter
+     * @param $defaut string The default value in case of non strict mode
+     * @return int The requested parameter as a integer
+     */
+    public function getNumber($name, $defaut=null) : int {
+        $value = $_REQUEST[$name] ?? null;
+        if(!is_numeric($value)) {
+            if($this->strict) {
+                $this->pebug->error("request::getNumber($name): The given parameter is not an integer");
+            } else {
+                return $defaut;
             }
         }
-        switch($this->type) {
-            case 'plaintext':
-                if($this->isValidated) return $this->returnValue;
-                else return $this->default;
-                break;
-            case 'free':
-                if($this->isValidated) return $this->returnValue;
-                else return $this->default;
-                break;
-            case 'email':
-                if($this->isValidated) return $this->returnValue;
-                else return $this->default;
-                break;
-            case 'number':
-                if($this->isValidated) return $this->returnValue;
-                else return $this->default;
-                break;
-            case 'array':
-                if(empty($this->array)) $this->pebug->exception("request::_get->NoArrayOptionsWhereGiven");
-                if($this->isValidated) return $this->returnValue;
-                else return $this->default;
-                break;
-        }
-        // No validation and not strict, maybe LOG?
-        return $this->returnValue;
+        return $value;
     }
 
-    // non strict classic
-    public function get($name, $type, $default='', $array=[]) {
-        switch($type) {
-            case 'PLAINTEXT':
-                return $this->name($name)->default($default)->validatePlaintext()->value();
-                break;
-            case 'TEXT':
-                return $this->name($name)->default($default)->validatePlaintext()->value();
-                break;
-            case 'FREE':
-                return $this->name($name)->default($default)->validateFree()->value();
-                break;
-            case 'EMAIL':
-                return $this->name($name)->default($default)->validateEmail()->value();
-                break;
-            case 'NUMBER':
-                return $this->name($name)->default($default)->validateNumber()->value();
-                break;
-            case 'ARRAY':
-                return $this->name($name)->default($default)->array($array)->validateArray()->value();
-                break;
+    /**
+     * @param $name string The name of the requested paraameter
+     * @param $defaut string The default value in case of non strict mode
+     * @return float The requested parameter as a float
+     */
+    public function getFloat($name, $defaut=null) : float {
+        $value = $_REQUEST[$name] ?? null;
+        if(!is_float($value)) {
+            if($this->strict) {
+                $this->pebug->error("request::getFloat($name): The given parameter is not an float");
+            } else {
+                return $defaut;
+            }
         }
-        return true;
+        return $value;
     }
 
+    /**
+     * @param $name string The name of the requested paraameter
+     * @param $defaut string The default value in case of non strict mode
+     * @return string The requested parameter as a string
+     */
+    public function getText($name, $defaut=null) : string {
+        $value = $_REQUEST[$name] ?? null;
+        if(!preg_match('/[0-9-a-z-A-Z]/',$value)) { // TODO: unit test
+            if($this->strict) {
+                $this->pebug->error("request::getText($name): The given parameter is not a plain text");
+            } else {
+                return $defaut;
+            }
+        }
+        return $value;
+    }
+
+    /**
+     * @param $name string The name of the requested paraameter
+     * @param $defaut string The default value in case of non strict mode
+     * @return string The requested parameter as a string (validated email)
+     */
+    public function getEmail($name, $defaut=null) : string {
+        $value = $_REQUEST[$name] ?? null;
+        if(!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            if($this->strict) {
+                $this->pebug->error("request::getEmail($name): The given parameter is not a plain text");
+            } else {
+                return $defaut;
+            }
+        }
+        return $value;
+    }
+
+    /**
+     * @param $name string The name of the requested paraameter
+     * @param array $options A list of parameters that can ge requested
+     * @param $defaut string The default value in case of non strict mode
+     * @return string One of the allowed requested options or the default value
+     */
+    public function getArray($name, $options=[], $defaut=nul) : string {
+        $value = $_REQUEST[$name] ?? null;
+        if(!is_array($options) || empty($options)) {
+            if($this->strict) {
+                $this->pebug->error("request::getArray($name): The given options are not valid");
+            } else {
+                return $defaut;
+            }
+        }
+        if(!in_array($value,$options)) {
+            if($this->strict) {
+                $this->pebug->error("request::getArray($name): The given parameter is not in the allowed array options");
+            } else {
+                return $defaut;
+            }
+        }
+        return $value;
+    }
+
+    /**
+     * @param $name string The name of the requested paraameter
+     * @param $defaut string The default value in case of non strict mode
+     * @return string The requested parameter as a string (parsed by XSS checker)
+     */
+    public function getFree($name, $defaut=null) : string {
+        $value = $_REQUEST[$name] ?? null;
+        $purifier = new HTMLPurifier();
+        $value = $purifier->purify($value);
+        if(empty($value)) { //TODO: intensive unit testing
+            if($this->strict) {
+                return $defaut;
+            }
+        }
+        return $value;
+    }
 
 }
